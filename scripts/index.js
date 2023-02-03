0c2c7137d1bce928a6273513c3e6ba92746da91e
Marilyn <oro.oro.dares@gmail.com>
2023-02-03 20:02:48 +0200

    Am adaugat un space

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const cl1 = document.querySelector('.cl1')
+const cl2 = document.querySelector('.cl2')
+const cl3 = document.querySelector('.cl3')
+const cl4 = document.querySelector('.cl4')
+
+cl1.addEventListener('click', () => {
+    cl1.classList.add('a')
+})
+cl2.addEventListener('click', () => {
+    cl2.classList.add('b')
+})
+cl3.addEventListener('click', () => {
+    cl3.classList.add('c')
+})
+cl4.addEventListener('click', () => {
+    cl4.classList.add('d')
+})
